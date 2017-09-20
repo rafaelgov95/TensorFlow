373df187c32dc629ce3bde1461ec6a7d5c1e28c9
Mr.Viana <rafaelgov95@gmail.com>
2017-09-19 20:19:47 -0400

    up

--- TensorFlow/Aula-I/Hello-Word.py
+++ TensorFlow/Aula-I/Hello-Word.py
@@ -1,10 +1,7 @@
-import numpy as np
 import graphviz
-from sklearn.datasets import load_iris
 from sklearn import tree
 
 v1 = [[140,1],[130,1],[150,0],[170,0],[150,1],[150,0]]
-# v1 = [[140,1],[130,1],[150,0],[170,0]]
 
 v2 = [0,0,1,1,0,1]
 
@@ -14,6 +11,11 @@ clf = clf.fit(v1,v2)
 
 print clf.predict([[150,0]])
 
-dot_data = tree.export_graphviz(clf, out_file = None)
+dot_data = tree.export_graphviz(clf, out_file=None,
+                         filled=True, rounded=True,
+                         special_characters=True)
 graph = graphviz.Source(dot_data)
-graph.render("frutas")
+
+graph.render('frutas')
+
+
