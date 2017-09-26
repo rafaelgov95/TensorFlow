833223a3acda3b8b3d47b69d8c612160150ecc6c
Mr.Viana <rafaelgov95@gmail.com>
2017-09-26 00:24:33 -0400

    up

--- Estudo-TensorFlow/Distribuido/Trainamento.py
+++ Estudo-TensorFlow/Distribuido/Trainamento.py
@@ -2,44 +2,19 @@
 # Autor: Rafael Viana
 # Codigo: Arvore de Decision Distribuida ADD
 import tensorflow as tf
-import numpy as np
-import graphviz
-from sklearn.datasets import load_iris
-from sklearn import tree
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-#
-# #Carrega o DataSet do Teste Iris.
-iris = load_iris()
-# # Cria Utiliza as Cluster para processar o dado.
-cluster = tf.train.ClusterSpec({"local": [ "svgov.ddns.net:3000","35.192.227.92:3001"]})
-#Divido os testes em Treinamento e Teste
-X_train ,X_teste , y_train , y_test = train_test_split(iris.data,iris.target,train_size=.018)
-print("Data de Teste\n",X_teste)
-print("Data Traning\n", X_train)
 
-# Serviço sendo executado no Cluster 1
+cluster = tf.train.ClusterSpec({"local": ["svgov.ddns.net:3000","35.192.227.92:3001"]})
+
+x = tf.constant(2)
+
+
 with tf.device("/job:local/task:1"):
-    clf = tree.DecisionTreeClassifier()
-    clf.fit(X_train, y_train)
-    a=clf.predict(X_teste)
+    y2 = x - 66
 
-# Serviço sendo executado no Cluster 0
 with tf.device("/job:local/task:0"):
-   print ('Resposta:\n', a)
-   print ('Correto:\n', y_test)
-   print ('Acertos: ',accuracy_score(y_test, a),'|','ESCALA: ',"'1.0 = 100%'")
+    y1 = x + 300
+    y = y1 + y2
 
-# Chamada de tf.Session()
-with tf.Session("grpc://svgov.ddns.net:3000") as sess:
-    with sess.as_default():
-        pass
-
-        # dot_data = tree.export_graphviz(clf, out_file=None,
-        #                                 feature_names=iris.feature_names,
-        #                                 class_names=iris.target_names,
-        #                                 filled=True, rounded=True,
-        #                                 special_characters=True)
-        # graph = graphviz.Source(dot_data)
-        #
-        # graph.render('iris')
+with tf.Session("grpc://svgov.ddns.net:3001") as sess:
+    result = sess.run(y)
+    print(result)
